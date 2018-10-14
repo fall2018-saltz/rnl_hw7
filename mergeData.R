b1c15cf6d1b62565404e2c440dcb71ce9d19c68d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-14 13:52:38 -0400

    Saving mergeData.R

--- mergeData.R
+++ mergeData.R
@@ -1,1 +1,14 @@
 
+# Copy the arrests data to a local variable
+arrests <- USArrests
+
+# Create a merged dataframe
+# Create a new column in arrests with state names
+arrests$State <- rownames(arrests)
+# Remove the District of Columbia from the census data 
+population <- clean_data[which(clean_data$stateName!="District of Columbia"),]
+# Combine the two dataframes
+arrestsPop <- cbind(arrests, population)
+# Remove the duplicate column
+arrestsPop <- arrestsPop[,-5]
+
