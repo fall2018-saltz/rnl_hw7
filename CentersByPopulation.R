71d6eec15cb2c88909c50621e2d050b4d0d64378
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-15 14:59:29 -0400

    Saving CentersByPopulation.R

--- CentersByPopulation.R
+++ CentersByPopulation.R
@@ -8,5 +8,4 @@ map.population <- ggplot(arrestsPopState, aes(map_id = tolower(state.name))) +
   geom_map (map=us, aes(fill=Murder)) +
   expand_limits(x=us$long, y=us$lat) +
   coord_map() +
-  ggtitle("US Map") +
-  geom_point(aes(x=arrestsPopState$x, y=arrestsPopState$y), size=arrestsPopState$Population)
+  ggtitle("US Map")
