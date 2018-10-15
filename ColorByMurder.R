b2c7bf7414618f89c3604fabaa920f6f66a87c9c
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-15 12:58:31 -0400

    Saving ColorByMurder.R

--- ColorByMurder.R
+++ ColorByMurder.R
@@ -1,2 +1,8 @@
 
 us <- map_data("state")
+
+map.murder <- ggplot(arrestsPopState, aes(map_id = tolower(state.name))) + 
+  geom_map (map=us, aes(fill=Murder)) +
+  expand_limits(x=us$long, y=us$lat) +
+  coord_map() +
+  ggtitle("US Map")
