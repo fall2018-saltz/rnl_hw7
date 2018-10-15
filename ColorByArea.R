f091298870dd208f1b8898f16b24e5d4d6d82559
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-15 12:54:25 -0400

    Saving ColorByArea.R

--- ColorByArea.R
+++ ColorByArea.R
@@ -1,1 +1,9 @@
 
+library(ggmap)
+library(ggplot2)
+
+map.area <- ggplot(arrestsPopState, aes(map_id = tolower(state.name))) + 
+  geom_map (map=us, aes(fill=state.area)) +
+  expand_limits(x=us$long, y=us$lat) +
+  coord_map()+
+  ggtitle("US Map")
