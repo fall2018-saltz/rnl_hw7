3c8747b6901b31738d556cdf101c0d8820b757f3
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-15 13:04:54 -0400

    Saving CentersByPopulation.R

--- CentersByPopulation.R
+++ CentersByPopulation.R
@@ -3,3 +3,6 @@ library(ggmap)
 library(ggplot2)
 
 us <- map_data("state")
+
+map.population <- map.murder +
+  geom_point(aes(x=arrestsPopState$x, y=arrestsPopState$y), size=arrestsPopState$Population/4000000)
