40f6f31c2198459ab2aa9330dc5e62751022b208
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 19:01:46 -0400

    Saving CentersZoomed.R

--- CentersZoomed.R
+++ CentersZoomed.R
@@ -8,6 +8,6 @@ map.zoomed <- ggplot(arrestsPopState, aes(map_id = tolower(state.name))) +
   geom_map (map=us, aes(fill=Murder)) +
   expand_limits(x=us$long, y=us$lat) +
   coord_map(xlim=c(-84.0060, -64.0060), ylim=c(30.7128, 50.7128)) +
-  ggtitle("US Map") +
+  ggtitle("New England State Centers by Population") +
   geom_point(aes(x=arrestsPopState$x, y=arrestsPopState$y), size=arrestsPopState$population/4000000)
 
