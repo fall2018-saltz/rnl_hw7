ce4210684be74434be0b136a3ca0f2f113a39350
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 12:16:54 -0400

    Saving CentersZoomed.R

--- CentersZoomed.R
+++ CentersZoomed.R
@@ -2,27 +2,14 @@
 library(ggmap)
 library(ggplot2)
 
+# Assign map data to a local variable
 us <- map_data("state")
 
-map.zoomed <- ggplot(arrestsPopState, aes(map_id = tolower(state.name))) +
-  geom_map (map=us, aes(fill=Murder)) +
-  expand_limits(x=us$long, y=us$lat) +
-  coord_map(xlim=c(-84.0060, -64.0060), ylim=c(30.7128, 50.7128)) +
-  ggtitle("New England State Centers by Population") +
-  geom_point(aes(x=arrestsPopState$x, y=arrestsPopState$y), size=arrestsPopState$population/4000000)
-
-
-library(ggmap)
-library(ggplot2)
-
-
-us <- map_data("state")
-
-# Add state centers (by population size) to map color-coded by murder rate
-map.population <- ggplot(arrestsPopState, aes(map_id = tolower(state.name))) + # Define plot
+# Zoom in on color-coded, map with sized state centers
+map.zoomed <- ggplot(arrestsPopState, aes(map_id = tolower(state.name))) + # Define plot
   geom_map (map=us, aes(fill=Murder)) + # Add map data and define fill (color)
   expand_limits(x=us$long, y=us$lat) + # Define map limits
-  coord_map() + # Ensure map coordinates are normalized
-  ggtitle("US State Centers by Population") + # Add title
+  coord_map(xlim=c(-84.0060, -64.0060), ylim=c(30.7128, 50.7128)) + # Define zoomed in map limits
+  ggtitle("New England State Centers by Population") + # Add title
   geom_point(aes(x=arrestsPopState$x, y=arrestsPopState$y), # Add state centers
     size=arrestsPopState$population/4000000) # Define size of centers as they relate to state population size
