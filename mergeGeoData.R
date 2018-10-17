84853395be3a33dceacdfa03e26bde75d270fb69
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 11:59:30 -0400

    Saving mergeGeoData.R

--- mergeGeoData.R
+++ mergeGeoData.R
@@ -1,7 +1,7 @@
 
 library(ggplot2)
 library(ggmap)
-arrestsPop$State <- rownames(arrestsPop)
+#arrestsPop$State <- rownames(arrestsPop)
 
 stateStats <- data.frame(state.name, state.center, state.area)
 arrestsPopState <- merge(arrestsPop, stateStats, by.x="State", by.y="state.name")
