282cf38b88b20e9783d6bd6e9d280cfcdc1c0a49
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 12:09:30 -0400

    Saving CentersByPopulation.R

--- CentersByPopulation.R
+++ CentersByPopulation.R
@@ -2,6 +2,7 @@
 library(ggmap)
 library(ggplot2)
 
+# Assign map data to a local variable
 us <- map_data("state")
 
 map.population <- ggplot(arrestsPopState, aes(map_id = tolower(state.name))) + 
@@ -14,7 +15,7 @@ map.population <- ggplot(arrestsPopState, aes(map_id = tolower(state.name))) +
 
 us <- map_data("state")
 
-# Create a color coded map by murder rate
+
 map.murder <- ggplot(arrestsPopState, aes(map_id = tolower(state.name))) + # Define plot
   geom_map (map=us, aes(fill=Murder)) + # Add map data and define fill (color)
   expand_limits(x=us$long, y=us$lat) + # Define map limits
