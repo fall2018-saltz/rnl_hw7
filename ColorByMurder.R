9c98cfd5df9b4218d9a3a0361604303febfa1112
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 12:07:51 -0400

    Saving ColorByMurder.R

--- ColorByMurder.R
+++ ColorByMurder.R
@@ -9,3 +9,14 @@ map.murder <- ggplot(arrestsPopState, aes(map_id = tolower(state.name))) +
   expand_limits(x=us$long, y=us$lat) +
   coord_map() +
   ggtitle("US States by Murder")
+
+
+
+us <- map_data("state")
+
+# Create a color coded map by state area
+map.area <- ggplot(arrestsPopState, aes(map_id = tolower(state.name))) + # Define plot
+  geom_map (map=us, aes(fill=state.area)) + # Add map data and define fill (color)
+  expand_limits(x=us$long, y=us$lat) + # Define map limits
+  coord_map() + # Ensure map coordinates are normalized
+  ggtitle("US States by Area") # Add title
