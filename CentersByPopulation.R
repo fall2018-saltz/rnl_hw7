582841c56e49c8d5c424ec4c157d9b9af7ea48ae
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 12:11:09 -0400

    Saving CentersByPopulation.R

--- CentersByPopulation.R
+++ CentersByPopulation.R
@@ -5,11 +5,12 @@ library(ggplot2)
 # Assign map data to a local variable
 us <- map_data("state")
 
-map.population <- ggplot(arrestsPopState, aes(map_id = tolower(state.name))) + 
-  geom_map (map=us, aes(fill=Murder)) +
-  expand_limits(x=us$long, y=us$lat) +
-  coord_map() +
-  ggtitle("US State Centers by Population") +
+# Add state centers (by population size) to map color-coded by murder rate
+map.population <- ggplot(arrestsPopState, aes(map_id = tolower(state.name))) + # Define plot
+  geom_map (map=us, aes(fill=Murder)) + # Add map data and define fill (color)
+  expand_limits(x=us$long, y=us$lat) + # Define map limits
+  coord_map() + # Ensure map coordinates are normalized
+  ggtitle("US State Centers by Population") + # Add title
   geom_point(aes(x=arrestsPopState$x, y=arrestsPopState$y), size=arrestsPopState$population/4000000)
 
 
@@ -17,7 +18,7 @@ us <- map_data("state")
 
 
 map.murder <- ggplot(arrestsPopState, aes(map_id = tolower(state.name))) + # Define plot
-  geom_map (map=us, aes(fill=Murder)) + # Add map data and define fill (color)
-  expand_limits(x=us$long, y=us$lat) + # Define map limits
-  coord_map() + # Ensure map coordinates are normalized
-  ggtitle("US States by Murder") # Add title
+  geom_map (map=us, aes(fill=Murder)) +
+  expand_limits(x=us$long, y=us$lat) +
+  coord_map() +
+  ggtitle("US States by Murder")
