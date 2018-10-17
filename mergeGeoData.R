1a644feb9068c99f7184b851e50123a99cbd5f4e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 12:02:59 -0400

    Saving mergeGeoData.R

--- mergeGeoData.R
+++ mergeGeoData.R
@@ -4,4 +4,6 @@ library(ggmap)
 
 # Create a new dataframe with geographical data
 stateStats <- data.frame(state.name, state.center, state.area)
+
+# Merge the geographical dataframe with the combined census / arrest dataframe
 arrestsPopState <- merge(arrestsPop, stateStats, by.x="State", by.y="state.name")
