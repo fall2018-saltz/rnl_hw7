10ef648bc1ce098707137ddd7ac1be3d9804ff0c
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 12:07:29 -0400

    Saving ColorByArea.R

--- ColorByArea.R
+++ ColorByArea.R
@@ -5,8 +5,9 @@ library(ggplot2)
 # Assign map data to a local variable
 us <- map_data("state")
 
-map.area <- ggplot(arrestsPopState, aes(map_id = tolower(state.name))) + 
-  geom_map (map=us, aes(fill=state.area)) +
-  expand_limits(x=us$long, y=us$lat) +
-  coord_map()+
-  ggtitle("US States by Area")
+# Create a color coded map by state area
+map.area <- ggplot(arrestsPopState, aes(map_id = tolower(state.name))) + # Define plot
+  geom_map (map=us, aes(fill=state.area)) + # Add map data and define fill (color)
+  expand_limits(x=us$long, y=us$lat) + # Define map limits
+  coord_map() + # Ensure map coordinates are normalized
+  ggtitle("US States by Area") # Add title
